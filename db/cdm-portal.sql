dccaa24cef34cf1f91702b31cedc79bd3dd9c432
imxaander <xander22.ison@gmail.com>
2024-04-24 12:14:46 +0800

    db update

--- db/cdm-portal.sql
+++ db/cdm-portal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 23, 2024 at 01:09 AM
+-- Generation Time: Apr 24, 2024 at 04:13 AM
 -- Server version: 11.3.2-MariaDB
 -- PHP Version: 8.3.3
 
@@ -24,28 +24,50 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `accounts`
+-- Table structure for table `course_enlistment`
 --
 
-CREATE TABLE `accounts` (
-  `account_id` varchar(25) NOT NULL,
-  `user_name` varchar(25) NOT NULL,
-  `password` varchar(25) NOT NULL
+CREATE TABLE `course_enlistment` (
+  `course_enlistment_id` varchar(10) NOT NULL,
+  `title` varchar(25) NOT NULL,
+  `type` varchar(3) NOT NULL,
+  `lab_units` int(11) NOT NULL,
+  `lec_units` int(11) NOT NULL,
+  `course_code` varchar(25) NOT NULL,
+  `version` varchar(5) NOT NULL,
+  `program` varchar(10) NOT NULL,
+  `year_level` varchar(5) NOT NULL,
+  `semester` varchar(5) NOT NULL,
+  `studio` varchar(25) NOT NULL,
+  `section` varchar(10) NOT NULL,
+  `day` varchar(10) NOT NULL,
+  `time` varchar(10) NOT NULL,
+  `room` varchar(25) NOT NULL,
+  `pre_co_requisite` varchar(50) NOT NULL,
+  `student_count` int(11) NOT NULL,
+  `academic_year` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `courses`
+-- Table structure for table `course_enrollment`
 --
 
-CREATE TABLE `courses` (
-  `course_id` varchar(10) NOT NULL,
-  `name` varchar(25) NOT NULL,
-  `type` varchar(3) NOT NULL,
-  `lab_units` int(11) NOT NULL,
-  `lec_units` int(11) NOT NULL,
-  `course_code` varchar(25) NOT NULL
+CREATE TABLE `course_enrollment` (
+  `course_enrollment_id` varchar(25) NOT NULL,
+  `student_id` varchar(25) NOT NULL,
+  `employee_id` varchar(25) NOT NULL,
+  `lec` int(11) NOT NULL,
+  `lab` int(11) NOT NULL,
+  `studio` varchar(25) NOT NULL,
+  `section` varchar(10) NOT NULL,
+  `day` varchar(20) NOT NULL,
+  `time` varchar(20) NOT NULL,
+  `room` varchar(10) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `version` varchar(10) NOT NULL,
+  `academic_year` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -64,18 +86,71 @@ CREATE TABLE `employees` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `enrollments`
+--
+
+CREATE TABLE `enrollments` (
+  `enrollment_id` varchar(25) NOT NULL,
+  `student_id` varchar(25) NOT NULL,
+  `academic_year` varchar(25) NOT NULL,
+  `semester` varchar(10) NOT NULL,
+  `program` varchar(25) NOT NULL,
+  `scholarship` varchar(50) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `date` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `students`
 --
 
 CREATE TABLE `students` (
   `student_id` varchar(25) NOT NULL,
-  `account_id` varchar(25) NOT NULL,
+  `user_name` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL,
   `first_name` varchar(25) NOT NULL,
   `last_name` varchar(25) NOT NULL,
   `age` int(11) NOT NULL,
   `program` varchar(100) NOT NULL,
   `courses` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`courses`))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`student_id`, `user_name`, `password`, `first_name`, `last_name`, `age`, `program`, `courses`) VALUES
+('student69', 'test', '1', 'Xander', 'Ison', 12, 'Bachelor of Science in Computer Engineering', '1');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `course_enlistment`
+--
+ALTER TABLE `course_enlistment`
+  ADD PRIMARY KEY (`course_enlistment_id`);
+
+--
+-- Indexes for table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`employee_id`);
+
+--
+-- Indexes for table `enrollments`
+--
+ALTER TABLE `enrollments`
+  ADD PRIMARY KEY (`enrollment_id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`student_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
